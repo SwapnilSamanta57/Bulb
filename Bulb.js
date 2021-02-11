08dacfde9c15053221532d6320a86e7adb68c5df
Swapnil <swapnilsamanta64@gmail.com>
2021-02-11 17:04:26 +0530

    first commit

---
+++ Bulb.js
@@ -0,0 +1,13 @@
+function turnon()
+{
+  var bulbimage=document.getElementById("bulbimage");
+  bulbimage.src="https://www.w3schools.com/js/pic_bulbon.gif";
+}
+
+function turnoff()
+{
+  var bulbimage=document.getElementById("bulbimage");
+  bulbimage.src="https://www.w3schools.com/js/pic_bulboff.gif";
+}
+
+  
